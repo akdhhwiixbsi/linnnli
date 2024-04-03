d62e52014bc377951fe1044834e32959cac3393e
akdhhwiixbsi <115683942+akdhhwiixbsi@users.noreply.github.com>
2024-04-03 10:08:53 +0800

    Add files via upload

---
+++ zj.js
@@ -0,0 +1,21 @@
+/******************************
+
+
+[rewrite_local]
+
+
+^https:\/\/*\.zhijiantime\.com\/.+ url script-response-body zj.js
+
+[mitm] 
+hostname = *.zhijiantime.com
+
+
+
+*******************************/
+
+var modifiedHeaders = $request.headers;
+
+modifiedHeaders['Authorization'] = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjI2MTI3IiwiYXVkIjoiWmhpamlhbkF1ZGllbmNlIiwibmJmIjoxNzExNjA0NjA4LCJTTlNQbGF0Zm9ybSI6IldYIiwiRGV2aWNlSWQiOiJBM0VFQzQ1NS00NjJCLURBM0UtMjdBRS0zNjdCRUE0NTRCMjUiLCJpc3MiOiJaaGlqaWFuemh1b3l1ZSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWUiOiLlk7zlk7zllKfllKciLCJleHAiOjU3OTMxNzMwMTh9.2rPoSGPRWFHSXJDFPYllKUwbvWmCD78XndLwgd-LwSY';
+
+
+$done({headers : modifiedHeaders});
