b6f71e6a9a3399519b559377b2b185d76d202040
akdhhwiixbsi <115683942+akdhhwiixbsi@users.noreply.github.com>
2024-04-05 21:16:12 +0800

    Add files via upload

---
+++ lyww.js
@@ -0,0 +1,30 @@
+/*************************************
+
+
+[rewrite_local]
+^https:\/\/partner-ai-api\.quthing\.com\/ai\/vip\/state url script-response-body lyww.js
+[mitm] 
+hostname = partner-ai-api.quthing.com
+
+
+*************************************/
+
+
+var obj = JSON.parse($response.body);
+
+obj.data = {
+    "dayCardBalance": 0,
+    "nextAutoRenewTime": 0,
+    "serverTime": 1695081521561,
+    "goodsId": 8888,
+    "seniorVip": true,
+    "showText": "会员尊享高级特权",
+    "validVip": true,
+    "expireTime": 2800012345000,
+    "vipCount": 180783,
+    "autoRenew": false,
+    "lastBuySkuName": ""
+  }
+}
+
+$done({body : JSON.stringify(obj)});
