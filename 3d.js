740f21c028604a3c6c88d032fdcedd4a1967e98e
akdhhwiixbsi <115683942+akdhhwiixbsi@users.noreply.github.com>
2024-04-29 10:05:55 +0800

    Add files via upload

---
+++ 3d.js
@@ -0,0 +1,16 @@
+\*******************************
+
+[rewrite_local]
+
+^https:\/\/app\.3dbody\.cn\/api\/(system\/register\/switch|course\/detail|content|common|resource|resource\/category\/list|body\/category\/list|system\/video\/watermark|body\/home\/body|content\/screen) url script-request-header 3d.js
+
+[mitm] 
+hostname = app.3dbody.cn
+
+*******************************/
+
+var modifiedHeaders = $request.headers;
+
+modifiedHeaders['authorization'] = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODY2MzM4MDc2OCIsImNyZWF0ZWQiOjE3MTQwMDg3NjE4NDMsImV4cCI6MTcyMTc4NDc2MX0.LhYVLsxTaal4vsPZ_BpouSx7RCQGcq8EQmB6KUUiUmXaFLpTJp_pzqsLzeir3e709ftE8AGZx5NsJ07tYqXbqQ';
+
+$done({headers : modifiedHeaders});
