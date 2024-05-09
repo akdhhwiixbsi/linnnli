8744c730192969ae65d4f2209b788649cb2c8416
akdhhwiixbsi <115683942+akdhhwiixbsi@users.noreply.github.com>
2024-05-09 11:32:14 +0800

    Add files via upload

---
+++ nm1.js
@@ -0,0 +1,17 @@
+/*********************************
+
+[rewrite_local]
+^https:\/\/fast\.lmfasting\.cn\/api\/User url script-response-body nm1.js
+[mitm] 
+hostname = fast.lmfasting.cn
+
+
+
+
+********************************/
+
+var body = $response.body;
+
+    body = body.replace(/\"cause":"减肥"/g,'\"cause":"减肥","vip_expire":"2099-12-31 18:10:32","flag":"permanent"');
+
+$done({body});
