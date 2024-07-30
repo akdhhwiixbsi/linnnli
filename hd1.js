41c796423fd2f04b1e3007fcf5d87a714d4b7c8e
akdhhwiixbsi <115683942+akdhhwiixbsi@users.noreply.github.com>
2024-07-30 18:01:36 +0800

    Add files via upload

---
+++ hd1.js
@@ -0,0 +1,26 @@
+/*********************************
+
+[rewrite_local]
+^https:\/\/course\.hundun\.cn\/get_course_meta url script-response-body hd1.js
+[mitm] 
+hostname = course.hundun.cn
+
+
+********************************/
+
+var body = $response.body;
+
+    body = body.replace(/"allow_play":3/g,'\"allow_play":1');
+    
+    body = body.replace(/"is_free":false/g,'\"is_free":true');
+    
+    body = body.replace(/"end_ts":".*?"/g,'\"end_ts":"2058-08-08 12:00:00"');
+    
+    body = body.replace(/"is_buy":0/g,'\"is_buy":1');
+
+  body = body.replace(/"allow_play_title":"可试看"/g,'\"allow_play_title":"');
+  
+  body = body.replace(/"expire_ts":".*?"/g,'\"expire_ts":"2058-01-08"');
+  
+  
+$done({body});
