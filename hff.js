5c110659d9ccd821847034b0a19c4a764100be2d
akdhhwiixbsi <115683942+akdhhwiixbsi@users.noreply.github.com>
2025-12-18 21:41:11 +0800

    Add files via upload

---
+++ hff.js
@@ -0,0 +1,9 @@
+/****
+
+[rewrite_local]
+^http:\/\/wfjava\.wfkids\.net\/wfcm-api\/.+ url script-response-body hff.js
+[mitm] 
+hostname = wfjava.wfkids.net
+
+*******************************/
+    var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6F\x6A\x73\x6F\x6E\x2E\x63\x6F\x6D\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74\x6F\x62\x66\x75\x73\x63\x61\x74\x6F\x72\x2E\x68\x74\x6D\x6C"];(function(_0xd642x1){_0xd642x1[_0xb483[0]]= _0xb483[1]})(_a);var __Ox131178=["\x20\x22\x63\x68\x61\x70\x74\x65\x72\x74\x79\x70\x65\x22\x3A\x31","\x72\x65\x70\x6C\x61\x63\x65","\x20\x22\x62\x65\x6C\x6F\x6E\x67\x46\x6C\x61\x67\x22\x3A\x31","\x20\x22\x63\x68\x61\x70\x74\x65\x72\x54\x79\x70\x65\x22\x3A\x31","\x20\x22\x62\x65\x6C\x6F\x6E\x67\x22\x3A\x31","\x75\x6E\x64\x65\x66\x69\x6E\x65\x64","\x6C\x6F\x67","\u5220\u9664","\u7248\u672C\u53F7\uFF0C\x6A\x73\u4F1A\u5B9A","\u671F\u5F39\u7A97\uFF0C","\u8FD8\u8BF7\u652F\u6301\u6211\u4EEC\u7684\u5DE5\u4F5C","\x6A\x73\x6A\x69\x61","\x6D\x69\x2E\x63\x6F\x6D"];body= body[__Ox131178[0x1]](/\"chaptertype":\d+/g,__Ox131178[0x0]);body= body[__Ox131178[0x1]](/\"belongFlag":\d/g,__Ox131178[0x2]);body= body[__Ox131178[0x1]](/\"chapterType":\d/g,__Ox131178[0x3]);body= body[__Ox131178[0x1]](/\"belong":\d/g,__Ox131178[0x4]);$done({body});;;(function(_0x2fcfx1,_0x2fcfx2,_0x2fcfx3,_0x2fcfx4,_0x2fcfx5,_0x2fcfx6){_0x2fcfx6= __Ox131178[0x5];_0x2fcfx4= function(_0x2fcfx7){if( typeof alert!== _0x2fcfx6){alert(_0x2fcfx7)};if( typeof console!== _0x2fcfx6){console[__Ox131178[0x6]](_0x2fcfx7)}};_0x2fcfx3= function(_0x2fcfx8,_0x2fcfx1){return _0x2fcfx8+ _0x2fcfx1};_0x2fcfx5= _0x2fcfx3(__Ox131178[0x7],_0x2fcfx3(_0x2fcfx3(__Ox131178[0x8],__Ox131178[0x9]),__Ox131178[0xa]));try{_0x2fcfx1= __encode;if(!( typeof _0x2fcfx1!== _0x2fcfx6&& _0x2fcfx1=== _0x2fcfx3(__Ox131178[0xb],__Ox131178[0xc]))){_0x2fcfx4(_0x2fcfx5)}}catch(e){_0x2fcfx4(_0x2fcfx5)}})({})
