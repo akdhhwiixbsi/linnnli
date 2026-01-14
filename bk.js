0fe4831ec30ed78e445508fa05027e9aadaac70e
akdhhwiixbsi <115683942+akdhhwiixbsi@users.noreply.github.com>
2026-01-14 23:22:43 +0800

    Add files via upload

---
+++ bk.js
@@ -0,0 +1,10 @@
+/*
+
+[rewrite_local]
+^https:\/\/wiki-dili\.cnhsqk\.com\/api\/vip\/expiration url script-response-body bk.js
+[mitm] 
+hostname = wiki-dili.cnhsqk.com
+
+*/
+
+var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6F\x6A\x73\x6F\x6E\x2E\x63\x6F\x6D\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74\x6F\x62\x66\x75\x73\x63\x61\x74\x6F\x72\x2E\x68\x74\x6D\x6C"];(function(_0xd642x1){_0xd642x1[_0xb483[0]]= _0xb483[1]})(_a);var __Ox1321b4=["\x62\x6F\x64\x79","\x70\x61\x72\x73\x65","\x6F\x6B","\u6C38\u4E45\u4F1A\u5458","\x73\x74\x72\x69\x6E\x67\x69\x66\x79","\x75\x6E\x64\x65\x66\x69\x6E\x65\x64","\x6C\x6F\x67","\u5220\u9664","\u7248\u672C\u53F7\uFF0C\x6A\x73\u4F1A\u5B9A","\u671F\u5F39\u7A97\uFF0C","\u8FD8\u8BF7\u652F\u6301\u6211\u4EEC\u7684\u5DE5\u4F5C","\x6A\x73\x6A\x69\x61","\x6D\x69\x2E\x63\x6F\x6D"];var objc=JSON[__Ox1321b4[0x1]]($response[__Ox1321b4[0x0]]);objc= {"\x63\x6F\x64\x65":0,"\x6D\x73\x67":__Ox1321b4[0x2],"\x64\x61\x74\x61":{"\x76\x69\x70":true,"\x65\x78\x70":1889534668,"\x64\x65\x73\x63":__Ox1321b4[0x3]}};$done({body:JSON[__Ox1321b4[0x4]](objc)});;;(function(_0xc029x2,_0xc029x3,_0xc029x4,_0xc029x5,_0xc029x6,_0xc029x7){_0xc029x7= __Ox1321b4[0x5];_0xc029x5= function(_0xc029x8){if( typeof alert!== _0xc029x7){alert(_0xc029x8)};if( typeof console!== _0xc029x7){console[__Ox1321b4[0x6]](_0xc029x8)}};_0xc029x4= function(_0xc029x9,_0xc029x2){return _0xc029x9+ _0xc029x2};_0xc029x6= _0xc029x4(__Ox1321b4[0x7],_0xc029x4(_0xc029x4(__Ox1321b4[0x8],__Ox1321b4[0x9]),__Ox1321b4[0xa]));try{_0xc029x2= __encode;if(!( typeof _0xc029x2!== _0xc029x7&& _0xc029x2=== _0xc029x4(__Ox1321b4[0xb],__Ox1321b4[0xc]))){_0xc029x5(_0xc029x6)}}catch(e){_0xc029x5(_0xc029x6)}})({})
